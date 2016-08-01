78498aacf84658932db2b4f9bd5fc95cdc642fe6
Andrea Wildsmith <awildsmi@hotmail.com>
2016-08-01 17:28:44 +0100

    Commit with 8.1: 1,3,5,7(ish),9

--- directory.rb
+++ directory.rb
@@ -4,33 +4,66 @@
 #and then print them
 def print_header
 puts "The students of Villians Academy"
-puts "--------------------------------"
+puts "---------"
 end
 
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "To finish, just hit return three times"
   students = []
   name = gets.chomp
+  puts "What cohort are they in?"
+  cohort = gets.chomp
+  puts "What country are they from?"
+  country = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: cohort, country: country}
+    if students.count > 1
+      puts "Now we have #{students.count} students"
+    else
+      puts "Now we have #{students.count} student"
+    end
+    puts "Enter another name"
     name = gets.chomp
+    puts "cohort"
+    cohort = gets.chomp
+    puts "country"
+    country = gets.chomp
   end
+    #if cohort.empty? do students << {cohort: november}
+    #end
+
   students
   end
 
   def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)"
+  @selected_students = []
+
+    students.map do |student|
+      if student[:name].length < 12
+        @selected_students << student
+        @selected_students.each  do |sel_student|
+          puts "#{sel_student[:name]}, (#{sel_student[:cohort]} cohort)"
+        end
+      end
+    end
   end
-end
+
+  #def print_footer(students)
+  #  puts "Looks like we have #{@selected_students.size} student(s) that have less than 12 characters in their names"
+  #end
+
 
 def print_footer(students)
-puts "Overall, we have #{students.count} great students"
+  if @selected_students.count >1
+puts "Overall, we have #{@selected_students.count} great students"
+else
+ puts "Overall, we have #{@selected_students.count} great student"
+end
 end
 
 students = input_students
+
 print_header
 print (students)
 print_footer(students)
