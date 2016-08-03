46821eed02ef32dcfd370bd67b4d2e488c33721a
Andrea Wildsmith <awildsmi@hotmail.com>
2016-08-03 09:52:46 +0100

    commit with more refactoring

--- directory.rb
+++ directory.rb
@@ -1,5 +1,5 @@
 
-
+@students = []
 
 #and then print them
 def print_header
@@ -8,66 +8,66 @@ puts "---------".center(90)
 end
 
 def interactive_menu
-  students =[]
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
     case selection
   when "1"
-    students = input_students
+  input_students
   when "2"
-    print_header
-    print(students)
-    print_footer(students)
+show_students
   when "9"
     exit
   else
     puts "I don't know what you meant, try again"
   end
 end
+
+
+  def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
   end
+
+  def show_students
+    print_header
+    print_students_list
+    print_footer
+  end
+
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
 end
 
 
-  def print(students)
- i = 0
- while i < students.length
-   students.each_with_index do |student, index|
+  def print_students_list
+   @students.each_with_index do |student, index|
        puts "#{index}: #{student[:name]}, from #{student[:country
        ]} in the (#{student[:cohort]} cohort)".center(90)
-i += 1
-       end
      end
  end
 
 
 
-def print_footer(students)
-  if students.count >1
-puts "Overall, we have #{students.size} great students".center(90)
+def print_footer
+  if @students.count >1
+puts "Overall, we have #{@students.size} great students".center(90)
 else
- puts "Overall, we have #{students.size} great student".center(90)
+ puts "Overall, we have #{@students.size} great student".center(90)
 end
 end
 
 interactive_menu
-students = input_students
-
-
-print_header
-print (students)
-print_footer(students)
