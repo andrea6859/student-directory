8f6e12d215ed1fa6f1e8ca82d419ede336d7b325
Andrea Wildsmith <awildsmi@hotmail.com>
2016-08-03 14:10:28 +0100

    more refactoring

--- directory.rb
+++ directory.rb
@@ -1,11 +1,11 @@
-@students = [] # an empty array accessible to all methods
+@students = []
 
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
-  puts "9. Exit" # 9 because we'll be adding more items
+  puts "9. Exit"
 end
 
 def interactive_menu
@@ -15,34 +15,25 @@ def interactive_menu
   end
 end
 
+
 def process(selection)
   case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "9"
-    exit # this will cause the program to terminate
-  when "3"
-    save_students
-  when "4"
-    load_students
-  else
-    puts "I don't know what you meant, try again"
+  when "1"; input_students
+  when "2"; show_students
+  when "9"; exit
+  when "3"; save_students
+  when "4"; load_students
+  else; puts "I don't know what you meant, try again"
   end
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # get the first name
   name = STDIN.gets.chomp
-  # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    # get another name from the user
     name = STDIN.gets.chomp
   end
 end
@@ -69,9 +60,7 @@ def print_footer
 end
 
 def save_students
-  # open the file for writing
   file = File.open("students.csv", "w")
-  # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
