3922da8bc3aa291b7c9b36464b069ab6f89a6a48
Andrea Wildsmith <awildsmi@hotmail.com>
2016-08-03 13:47:22 +0100

    loads students.csv by default

--- directory.rb
+++ directory.rb
@@ -1,10 +1,11 @@
+@students = [] # an empty array accessible to all methods
 
-@students = []
-
-#and then print them
-def print_header
-puts "The students of Villians Academy".center(90)
-puts "---------".center(90)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items
 end
 
 def interactive_menu
@@ -15,79 +16,62 @@ def interactive_menu
 end
 
 def process(selection)
-    case selection
+  case selection
   when "1"
-  input_students
+    input_students
   when "2"
-show_students
-when "3"
-  save_students
-when "4"
-  load_students
+    show_students
   when "9"
-    exit
+    exit # this will cause the program to terminate
+  when "3"
+    save_students
+  when "4"
+    load_students
   else
     puts "I don't know what you meant, try again"
   end
 end
 
-
-  def print_menu
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "3. Save the list to students.csv"
-    puts "4. Load the list from students.csv"
-    puts "9. Exit"
-  end
-
-  def show_students
-    print_header
-    print_students_list
-    print_footer
-  end
-
-  def try_load_students
-    filename = ARGV.first
-    return if filename.nil?
-    if File.exists?(filename)
-      load_students(filename)
-       puts "Loaded #{@students.count} from #{filename}"
-    else
-      puts "Sorry, #{filename} doesn't exist."
-      exit
-    end
-  end
-
-def load_students
-  file=File.open("students.csv", "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
+  # get the first name
   name = STDIN.gets.chomp
+  # while the name is not empty, repeat this code
   while !name.empty? do
+    # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
+    # get another name from the user
     name = STDIN.gets.chomp
   end
 end
 
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
 
-  def print_students_list
-   @students.each_with_index do |student, index|
-       puts "#{index}: #{student[:name]}, from #{student[:country
-       ]} in the (#{student[:cohort]} cohort)".center(90)
-     end
- end
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
 
 def save_students
+  # open the file for writing
   file = File.open("students.csv", "w")
+  # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
@@ -96,12 +80,25 @@ def save_students
   file.close
 end
 
-def print_footer
-  if @students.count >1
-puts "Overall, we have #{@students.size} great students".center(90)
-else
- puts "Overall, we have #{@students.size} great student".center(90)
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
+
+def try_load_students
+  filename = ARGV.first
+  filename = "students.csv" if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
 end
 
 try_load_students
